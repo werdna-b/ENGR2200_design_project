0e0539844944ff4da4c8b9b83239db44a87af5cd
Andrew Bargen <andrew.bargen872@hotmail.com>
2025-11-16 21:42:05 -0700

    add module x, update design_top, .gitignore

--- final_project/final_project.srcs/sources_1/new/design_top.v
+++ final_project/final_project.srcs/sources_1/new/design_top.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/15/2025 09:45:26 PM
-// Design Name: 
-// Module Name: design_top
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module design_top(
 
---
+++ final_project/final_project.srcs/sources_1/new/x.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+
+// how to ensure fire isn't high for two consecutive clock cycles?
+
+// update case statement if these values are changed
+`define VDC_VECTORNUM 4 // always equal to COUNT_WIRES ^ 2
+`define COUNT_WIRES 2 
+
+module x(
+    input rst, clk, row_en, col_en, add_n, fire,
+    output to_vdc
+    );
+
+    wire add_en;
+    assign add_en = fire & (row_en | col_en);
+
+    reg [COUNT_WIRES-1:0] count;
+
+    always @(posedge clk) begin
+        if (rst) count <= 0;
+        if (add_en) begin
+            if (~add_n) count <= count + 1;
+            else count <= count - 1;
+        end
+    end
+
+    always @(*) begin
+        case (count)
+            2'b00: to_vdc = 4'b0001;
+            2'b01: to_vdc = 4'b0010;
+            2'b10: to_vdc = 4'b0100;
+            2'b11: to_vdc = 4'b1000;
+            default: to_vdc = 4'bx;
+        endcase
+    end
+
+endmodule
