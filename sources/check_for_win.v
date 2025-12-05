987d484cdb09c36c47c22cef10059af1ec82aff9
HydroRacer <hydroracer76975@gmail.com>
2025-12-04 19:12:24 -0700

    my terrible check at the moment just saving

---
+++ sources/check_for_win.v
@@ -0,0 +1,35 @@
+module check_for_win #(parameter NumberOfBits = 31)(
+    input clk,
+    input ScreenValues [NumberOfBits:0],
+    input reset,
+    output Buzz
+);
+integer count;
+integer ForCount;
+reg WinCondition;
+
+always @(posedge clk) begin
+
+   for (count = 0; count <= NumberOfBits; count = count + 1) begin
+        if (ScreenValues [count] == 0)
+            ForCount = ForCount + 1;
+        else 
+            ForCount = 0;
+            
+            if(ForCount == NumberOfBits)
+                assign WinCondition = 1;
+        end
+         
+         
+    if (ForCount == NumberOfBits)
+      assign  WinCondition = 1;
+    end
+    
+    always @(posedge clk) begin
+        if (reset == 1)
+            assign WinCondition = 0;
+end
+
+    
+endmodule 
+    
