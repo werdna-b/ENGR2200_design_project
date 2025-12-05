9f1cc4f94353804957164bb639b24a7893b79508
HydroRacer <hydroracer76975@gmail.com>
2025-12-04 21:22:08 -0700

    Trying again

--- sources/Shuffle and solve state.v
+++ sources/Shuffle and solve state.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/02/2025 08:30:05 AM
-// Design Name: 
-// Module Name: Lets_go
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 /*
 
 module Lets_go(
