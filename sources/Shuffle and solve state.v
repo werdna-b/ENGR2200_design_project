4e3718526db65eda08c7a33bc9cbb04b59021d0a
HydroRacer <hydroracer76975@gmail.com>
2025-12-04 21:41:52 -0700

    a bundle of little updates

--- sources/Shuffle and solve state.v
+++ sources/Shuffle and solve state.v
@@ -1,33 +1,23 @@
 `timescale 1ns / 1ps
-/*
+
 
 module Lets_go(
     input clk,          // 100 MHz System Clock (Pin W5)
-    input sw15,
-    input btnC,
-    output audio_out,   // Audio Signal (Pin JA1)
-    output amp_gain,    // Gain Control (Pin JA2)
-    output amp_shdn    // Shutdown Control (Pin JA3)
+    input mix_state,
+    input btnC
     );
     
+    always @(posedge clk) begin
     
-    reg [15:0] count;
-    count = 'b0000000000000000;
     
-    if (sw15 == 1)
-        always @(posedge clk ) begin
-            
-            count <= count + 1; // Count up
-        end
-    else 
-        count = 0;
+    
+    end
+    
+    
+    
 
-    
-    
-    
+
+
+
+
 endmodule
-
-
-module adder(
-    input 
-    */
